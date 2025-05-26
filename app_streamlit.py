4406e90c2a17ef00875a9e5277c6494f4c24604a
Afif A. Iskandar <surel.afifai@gmail.com>
2025-05-26 21:15:50 +0700

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -1,3 +1,12 @@
+from transformers import pipeline
 import streamlit as st
 st.title("aplikasi sederhana")
 st.write("ini adalah aplikasi sederhana")
+# Buat pipeline NLP untuk analisis sentimen
+classifier = pipeline("sentiment-analysis")
+
+text_input = st.text_input("Insert a sentence")
+if st.button("submit"):
+# Analisis kalimat
+    result = classifier(text_input)
+    st.json(result)
